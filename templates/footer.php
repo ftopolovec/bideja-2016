c7302e16c91e44112fb84591921843e5aec567cb
BurzaIdeja <filip@burza-ideja.hr>
2016-07-21 15:53:30 +0200

    Dodani svi ključni elementi na početnu-treba finiširati

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,39 @@
 <footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+	<div class="container-fluid bg-blue">
+	  <div class="container top-marg-50 bottom-marg-50">
+	    <div class="col-md-5">
+	    	<h3>O nama</h3>
+	    	<hr align="left">
+	    	<p >Burza ideja djeluje od 2008. godine kao specijalizirana tvrtka za izradu i održavanje responzivnih web stranica i web trgovina, izradu SEO analiza i SEO optimizacije web stranica.</p>
+	    	<hr class="footer-separator">
+	    	<p>Copyright 2016 - Burza Ideja</p>
+	    </div>
+	    <div class="col-md-7 col-separator-footer">
+	    	<h3>Kontakt podaci</h3>
+	    	<hr align="left">
+	    	<p><span></span> Burza ideja d.o.o., Krapinska 9, 42000 Varaždin OIB: 60382139644<br>
+ <span></span> +385 95 910 9102 – Mladen  <span></span> +385 91 186 5265 – Josip  
+ <span></span> info@burza-ideja.hr</p>
+ 			<h3>Prijava na Newsletter</h3>
+	    	<hr align="left">
+	    	 <div class="input-group">
+		      <input type="text" class="form-control" placeholder="Upišite vasu-email@adresu.com">
+		      <span class="input-group-btn">
+		        <button class="btn btn-default" type="button">Pretplati me</button>
+		      </span>
+		    </div><!-- /input-group -->
+	    </div>
+	  </div>
+	</div>
 </footer>
+
+<?php if(is_front_page()){ ?>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/hr_HR/sdk.js#xfbml=1&version=v2.7&appId=1543406099223777";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<?php } ?>
--- templates/partials/front-page.php
+++ templates/partials/front-page.php
@@ -1,9 +1,125 @@
 <?php get_template_part('templates/partials/slider-front', 'page'); ?>
 <div class="container">
 	<div class="row top-marg-50">
-		<div class="col-md-3" align="center"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/iws.jpg"></div>
-		<div class="col-md-3" align="center"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/iwt.jpg"></div>
-		<div class="col-md-3" align="center"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/seo.jpg"></div>
-		<div class="col-md-3" align="center"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/diz.jpg"></div>
+		<div class="col-md-3" align="center">
+			<img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/iws.jpg">
+			<h3>Izrada web stranica</h3>
+			<p>U izradi web stranica imamo višegodišnje iskustvo i preko stotinjak zadovoljnih klijenata.</p>
+			<a href="#">Saznaj više</a>
+		</div>
+		<div class="col-md-3" align="center">
+			<img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/iwt.jpg">
+			<h3>Izrada web trgovina</h3>
+			<p>Nudimo kompletno rješenje izrade responzivnih web trgovina koje možete potpuno sami ažurirati!</p>
+			<a href="#">Saznaj više</a>
+		</div>
+			<div class="col-md-3" align="center">
+			<img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/seo.jpg">
+			<h3>SEO analiza i optimizacija</h3>
+			<p>Nudimo cjelovitu SEO analizu i optimizaciju vaše web stranice ili trogovine za najbolje položaje na Google-u!</p>
+			<a href="#">Saznaj više</a>
+		</div>
+		<div class="col-md-3" align="center">
+			<img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/diz.jpg">
+			<h3>Grafički dizajn</h3>
+			<p>Grafički dizajn nošen na leđima IDEJA koje pristaju Vašem projektu!</p>
+			<a href="#">Saznaj više</a>
+		</div>
+	</div>
+	<div class="row top-marg-50">
+		<div class="col-md-7"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/monitor-mobitel-grafika.jpg" class="img-responsive"></div>
+		<div class="col-md-5">
+			<h1>Burza ideja - ukratko</h1>
+			<h4>Mi smo kreativan i ambiciozan tim sa puno ideja 
+spremnih za poboljšanje vašeg poslovanja!</h4>
+			<hr align="left">
+			<p>Burza ideja djeluje od 2008. godine kao specijalizirana tvrtka za izradu i održavanje responzivnih web stranica i web trgovina, izradu SEO analiza i SEO optimizacije web stranica.</p>
+			<a href="#" class="hvr-icon-forward">Saznaj nešto više</a>
+		</div>
+	</div>
+</div>
+<div class="container-fluid bg-green">
+	<div class="container top-marg-50 bottom-marg-50">
+		<div class="col-md-12 text-center">
+			<h2>Izradit ćemo Vam neobvezujuću ponudu!</h2>
+			<hr>
+			<h3>Pošaljite nam upit, objasnite čime se bavite i što biste željeli<br> te ćemo Vam izraditi prikladnu neobvezujuću ponudu.</h3>
+		</div>
+		<div class="col-md-12" align="center"><a href="#" class="hvr-icon-forward">Zatražite ponudu</a></div>
+	</div>
+</div>
+<div class="container-fluid bg-blue">
+	<div class="container top-marg-50 bottom-marg-50 text-center">
+		<div class="row">
+			<div class="col-md-12">
+				<h2>Projekti na kojima smo radili</h2>
+				<hr>
+				<h3>Svakom projektu posvećujemo posebnu pažnju kako<br> bi poboljšali poslovanje naših klijenata</h3>
+			</div>
+		</div>
+		<div class="row top-marg-50">
+			<div class="col-md-4">
+				<img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/projekti/3dental-thumb.jpg" class="img-responsive">
+				<a href="#" class="project-more-plus"><span></span></a>
+				<a href="#" class="project-box-bot">3Dentall</a>
+			</div>
+			<div class="col-md-4">
+				<img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/projekti/3dental-thumb.jpg" class="img-responsive">
+				<a href="#" class="project-more-plus"><span></span></a>
+				<a href="#" class="project-box-bot">3Dentall</a>
+			</div>
+			<div class="col-md-4">
+				<img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/projekti/3dental-thumb.jpg" class="img-responsive">
+				<a href="#" class="project-more-plus"><span></span></a>
+				<a href="#" class="project-box-bot">3Dentall</a>
+			</div>
+		</div>
+		<div class="col-md-12 top-marg-50" align="center"><a href="#" class="hvr-icon-forward">Svi projekti</a></div>
+	</div>
+</div>
+<div class="container-fluid bg-light-blue">
+	<div class="container top-marg-50 bottom-marg-50">
+		<div class="col-md-8">
+			<h2>Novo s našeg bloga</h2>
+			<hr align="left">
+			<ul class="blog-headline">
+				<li class="date-blog">09</li>
+				<li class="month-year-blog">LIP<br>2016</li>
+				<li class="title-blog-frontpage"><h3>Front-end development</h3></li>
+			</ul>
+			<ul class="fa-list">
+				<li><span></span> Admin</li>
+				<li><span></span> Marketing</li>
+			</ul>
+			<img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/blog/frontend-thumb.jpg" class="img-responsive">
+			<p>Front-end development je dio procesa izrade web stranice koji se odnosi na sve elemente dostupne krajnjem korisniku. Osoba koja se bavi tim poslom naziva se Front-end developer i odgovorna je za izradu svega vidljivog na web stranici, raspored elemenata, funkcionalnost i korisnički doživljaj (eng. User Experience – UX).</p>
+			<a href="#" class="hvr-icon-forward">Saznaj nešto više</a>
+		</div>
+		<div class="col-md-4">
+			<h2>Facebook aktivnosti</h2>
+			<hr align="left">
+			<div class="fb-page" data-href="https://www.facebook.com/burza.ideja/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/burza.ideja/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/burza.ideja/">Burza ideja Facebook aktivnosti</a></blockquote></div>
+
+		</div>
+	</div>
+</div>
+<div class="container-fluid">
+	<div class="container top-marg-50 bottom-marg-50 clients-wrapper">
+	<div class="row top-marg-50">
+		<div class="col-md-2"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/klijenti/3dentall.jpg"></div>
+		<div class="col-md-2"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/klijenti/mag-commerce.jpg"></div>
+		<div class="col-md-2"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/klijenti/zupanijska-bolnica-cakovec.jpg"></div>
+		<div class="col-md-2"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/klijenti/noah-international.jpg"></div>
+		<div class="col-md-2"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/klijenti/mah-sport.jpg"></div>
+		<div class="col-md-2"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/klijenti/catpaw.jpg"></div>
+	</div>
+	<div class="row top-marg-50 ">
+		<div class="col-md-2"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/klijenti/centar-poslovniih-analiza.jpg"></div>
+		<div class="col-md-2"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/klijenti/poliel.jpg"></div>
+		<div class="col-md-2"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/klijenti/vina-preiner.jpg"></div>
+		<div class="col-md-2"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/klijenti/editer.jpg"></div>
+		<div class="col-md-2"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/klijenti/poljodom-ivanec.jpg"></div>
+		<div class="col-md-2"><img src="<?= esc_url(get_template_directory_uri ()); ?>/assets/images/klijenti/tectum.jpg"></div>
+	</div>
 	</div>
 </div>
